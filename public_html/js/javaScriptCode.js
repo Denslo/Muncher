83706a6870c5231bfce77683c7d19d06f135c6fa
Ran Klein <ranklein86@gmail.com>
2014-04-17 02:35:04 +0300

    all we need now is the tables

--- public_html/js/javaScriptCode.js
+++ public_html/js/javaScriptCode.js
@@ -24,72 +24,88 @@ function toggleTable(tID) {
 }
 
 $(function() {
+    $('#addres').on("pageshow", function() {
 
-    navigator.geolocation.getCurrentPosition(function(pos) {
+        navigator.geolocation.getCurrentPosition(function(pos) {
 
-        var lat = pos.coords.latitude;
-        var lng = pos.coords.longitude;
-        var latlng = new google.maps.LatLng(lat, lng);
-
-        var options = {
-            map: "#map_canvas",
-            location: latlng,
-            markerOptions: {
-                draggable: true
-            },
-            mapOptions: {
-                zoom: 18
-            }
-        };
-
-        $("#geocomplete").geocomplete(options);
-        geocoder = new google.maps.Geocoder();
-        geocoder.geocode({
-            latLng: latlng
-        }, function(responses) {
-            if (responses && responses.length > 0) {
-                $('#geocomplete').val(responses[0].formatted_address);
-            }
-        });
-
-        $("#geocomplete").bind("geocode:dragged", function(event, latLng) {
+            var lat = pos.coords.latitude;
+            var lng = pos.coords.longitude;
+            var latlng = new google.maps.LatLng(lat, lng);
+            var options = {
+                map: "#map_canvas",
+                location: latlng,
+                markerOptions: {
+                    draggable: true
+                },
+                mapOptions: {
+                    zoom: 18
+                }
+            };
+            $("#geocomplete").geocomplete(options);
             geocoder = new google.maps.Geocoder();
             geocoder.geocode({
-                latLng: latLng
+                latLng: latlng
             }, function(responses) {
                 if (responses && responses.length > 0) {
                     $('#geocomplete').val(responses[0].formatted_address);
-                } else {
-                    $('#geocomplete').val('Cannot determine address at this location.');
                 }
             });
-        });
-
-
-    }, function(pos) {
-        var options = {
-            map: "#map_canvas",
-            location: "Israel, Tel Aviv",
-            markerOptions: {
-                draggable: true
-            },
-        };
-
-        $("#geocomplete").geocomplete(options);
-
-        $("#geocomplete").bind("geocode:dragged", function(event, latLng) {
-            geocoder = new google.maps.Geocoder();
-            geocoder.geocode({
-                latLng: latLng
-            }, function(responses) {
-                if (responses && responses.length > 0) {
-                    $('#geocomplete').val(responses[0].formatted_address);
-                } else {
-                    $('#geocomplete').val('Cannot determine address at this location.');
+            $("#geocomplete").bind("geocode:dragged", function(event, latLng) {
+                geocoder = new google.maps.Geocoder();
+                geocoder.geocode({
+                    latLng: latLng
+                }, function(responses) {
+                    if (responses && responses.length > 0) {
+                        $('#geocomplete').val(responses[0].formatted_address);
+                    }
+                });
+            });
+        }, function() {
+            var options = {
+                map: "#map_canvas",
+                location: "Israel, Tel Aviv",
+                markerOptions: {
+                    draggable: true
                 }
+            };
+            $("#geocomplete").geocomplete(options);
+            $("#geocomplete").bind("geocode:dragged", function(event, latLng) {
+                geocoder = new google.maps.Geocoder();
+                geocoder.geocode({
+                    latLng: latLng
+                }, function(responses) {
+                    if (responses && responses.length > 0) {
+                        $('#geocomplete').val(responses[0].formatted_address);
+                    }
+                });
             });
         });
     });
 });
+function submitOrder() {
+}
 
-function submitOrder(){}
+$(function() {
+    $(document).on("pageinit", "#surprise_box", function() {
+        $(document).on("swipeleft swiperight", "#surprise_box", function(e) {
+            if ($.mobile.activePage.jqmData("panel") !== "open") {
+                if (e.type === "swipeleft") {
+                    $("#right-panel").panel("open");
+                }
+            }
+        });
+    });
+    $(document).on("pageinit", "#addres", function() {
+        $(document).on("swipeleft swiperight", "#addres", function(e) {
+            if ($.mobile.activePage.jqmData("panel") !== "open") {
+                if (e.type === "swipeleft") {
+                    $("#right-panel").panel("open");
+                }
+            }
+        });
+    });
+});
+
+$(function() {
+    $('#map_canvas').height($(window).height() * 0.65);
+})
