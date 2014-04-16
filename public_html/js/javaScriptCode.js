485904b185d3e26060bec1accebcbef7c8704187
Ran Klein <ranklein86@gmail.com>
2014-04-16 12:37:26 +0300

    aaa

---
+++ public_html/js/javaScriptCode.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function login(){$.mobile.changePage("#page_order_type", {transition: "slide"});}
+
+function goToOrderFromList(){$.mobile.changePage("#shopping_list", {transition: "slide"});}
+
+function goToOrderSurpriseBox(){$.mobile.changePage("#surprise_box", {transition: "slide"});}
+
+function goToAddres(){$.mobile.changePage("#addres", {transition: "slide"});}
+
+function toggleTable(tID){$(tID).slideToggle();}
+
+$(document).ready(function() {
+    
+});
+
+
