e72c9bd0fce333839855889d8e2cc8e2efe254ed
Ran Klein <ranklein86@gmail.com>
2014-05-07 18:57:54 +0300

    add some css

--- server/muncherServer/routes/muncher.js
+++ server/muncherServer/routes/muncher.js
@@ -1,24 +1,36 @@
 var express = require('express');
 var router = express.Router();
 /*
- router.get('/insertToDB', function(req, res) {
- var db = req.db;
- var collection = db.get('pordactcolection');
- 
- var document = {
- name: "para",
- img: "/images/para.jpg",
- price: 70,
- stock: 100
- };
- 
- collection.insert(document, function(err, doc) {
- if (err === null) {
- res.send(doc._id);
- }
- });
- });
- */
+router.get('/insertToDB', function(req, res) {
+    var db = req.db;
+    var collection = db.get('pordactcolection');
+
+    var document = [{
+            name: "para",
+            img: "/images/para.jpg",
+            price: 70,
+            stock: 100
+        }, {
+            name: "bamba",
+            img: "/images/bamba.jpg",
+            price: 50,
+            stock: 100
+        }, {
+            name: "bisli",
+            img: "/images/bisli.jpg",
+            price: 10,
+            stock: 100
+        }];
+
+    document.forEach(function(e) {
+        collection.insert(e, function(err, doc) {
+            if (err === null) {
+                res.send(doc._id);
+            }
+        });
+    })
+});*/
+
 
 router.get('/getProdacts', function(req, res) {
     var db = req.db;
